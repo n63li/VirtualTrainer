be4e19d19e40842c4420549f4b21b714a1252b7c
Nathan Li <n63li@edu.uwaterloo.ca>
2021-02-25 22:56:04 -0500

    Pushing for debugging

---
+++ VirtualTrainer/WorkoutResultViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutResultViewController.swift
+//  VirtualTrainer
+//
+//  Created by Nathan Li on 2021-02-25.
+//  Copyright © 2021 Google Inc. All rights reserved.
+//
+
+import UIKit
+
+@objc(WorkoutResultViewController)
+class WorkoutResultViewController: UIViewController {
+  @IBOutlet weak var typeLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var accuracyLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
