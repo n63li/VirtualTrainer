25b0e54e1f27dcc6138d13017879de0b0785eb7a
Nathan Li <n63li@edu.uwaterloo.ca>
2021-03-01 19:00:29 -0500

    Adding date label in feedback view

--- VirtualTrainer/FeedbackViewController.swift
+++ VirtualTrainer/FeedbackViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 @objc(FeedbackViewController)
 class FeedbackViewController: UIViewController {
-    var workoutSession: WorkoutSession? = nil
-    @IBOutlet weak var doneButton: UIBarButtonItem!
+  var workoutSession: WorkoutSession? = nil
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var doneButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+        let date =  Date(timeIntervalSince1970: workoutSession?.startTimestamp ?? 0)
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone(abbreviation: "EST") //Set timezone that you want
+        dateFormatter.locale = NSLocale.current
+        dateFormatter.dateFormat = "EEEE, MMM d" //Specify your format that you want
+        let strDate = dateFormatter.string(from: date)
+        
+        dateLabel?.text = strDate
     }
     
     @IBAction func finish(_ sender: Any) {
