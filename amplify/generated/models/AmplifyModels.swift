bde73e6f99acc3db29e8ca34d3ccbacd9a8bae89
Henry Xu <henry.m.xu@live.com>
2021-03-03 18:04:50 -0500

    Update Amplify Models

--- amplify/generated/models/AmplifyModels.swift
+++ amplify/generated/models/AmplifyModels.swift
@@ -5,11 +5,9 @@ import Foundation
 // Contains the set of classes that conforms to the `Model` protocol. 
 
 final public class AmplifyModels: AmplifyModelRegistration {
-  public let version: String = "e1e74c94bc447e2580c143f638ceb670"
+  public let version: String = "b974932b173884afe5daef5e07f85071"
   
   public func registerModels(registry: ModelRegistry.Type) {
     ModelRegistry.register(modelType: WorkoutSessionModel.self)
-    ModelRegistry.register(modelType: WorkoutResultModel.self)
-    ModelRegistry.register(modelType: IdealWorkoutModel.self)
   }
 }
