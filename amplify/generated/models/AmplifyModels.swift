4a3af5b6c5bd5ea2b5553ed0720f92ade8dc2e58
Nathan Li <n63li@edu.uwaterloo.ca>
2021-03-03 13:27:29 -0500

    Adding videoURL field to workout session model

--- amplify/generated/models/AmplifyModels.swift
+++ amplify/generated/models/AmplifyModels.swift
@@ -5,7 +5,7 @@ import Foundation
 // Contains the set of classes that conforms to the `Model` protocol. 
 
 final public class AmplifyModels: AmplifyModelRegistration {
-  public let version: String = "2b222191cbd877bd9e3b5b85453fcc80"
+  public let version: String = "e1e74c94bc447e2580c143f638ceb670"
   
   public func registerModels(registry: ModelRegistry.Type) {
     ModelRegistry.register(modelType: WorkoutSessionModel.self)
