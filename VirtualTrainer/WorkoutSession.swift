98901472c65a227e150da491350ee1e1909e4a7c
Nathan Li <n63li@edu.uwaterloo.ca>
2021-03-05 16:56:05 -0500

    Tuning scoring algorithm

--- VirtualTrainer/WorkoutSession.swift
+++ VirtualTrainer/WorkoutSession.swift
@@ -16,7 +16,7 @@ class WorkoutSession {
   var endTimestamp: Double = 0
   var squatElements: [SquatElement] = []
   var deadliftElements: [DeadliftElement] = []
-    var videoURL: String? = ""
+  var videoURL: String? = ""
   
   init(workoutType: String, cameraAngle: WorkoutOrientation) {
     self.workoutType = workoutType
@@ -26,54 +26,59 @@ class WorkoutSession {
   }
   
   func calculateScore() throws {
-      var idealWorkouts = [
-       "squat": Squat()
-      ] as [String : IdealWorkout]
+    var idealWorkouts = [
+      "squat": Squat()
+    ] as [String : IdealWorkout]
     
-      let idealWorkout = idealWorkouts[self.workoutType]!.jointAngles[self.cameraAngle]
-      print(idealWorkout)
-      var currentIdealFrameIndex = 0
-      let keys = try self.squatElements[0].allProperties()
-    let tolerance = 5.0
-      var score = 100.0
+    let idealWorkout = idealWorkouts[self.workoutType]!.jointAngles[self.cameraAngle]
+    print(idealWorkout)
+    var currentIdealFrameIndex = 0
+    let keys = try self.squatElements[0].allProperties()
+    var tolerance = 4.0
+    var score = 100.0
     print(self.squatElements.count)
     print(self.squatElements[0])
+    var iterations = 1
+    while currentIdealFrameIndex < idealWorkout!.count {
+      tolerance += Double(iterations) * 2
       for userFrame in self.squatElements {
         if currentIdealFrameIndex >= idealWorkout!.count {
-          print("current ideal frame index out of bounds")
           break
         }
         let currentIdealFrame = idealWorkout![currentIdealFrameIndex]
         var isUserFrameGood = false
         var scoreDeduction = 0.0
-        let scoreDeductionWeighting = 0.5
+        let scoreDeductionWeighting = 0.25
         for (key, _): (String, Any) in keys {
           let userVal = userFrame.valueByPropertyName(name: key)
           let idealVal = currentIdealFrame.valueByPropertyName(name: key)
-           let difference = Double(abs(userVal - idealVal))
-           if (difference <= tolerance) {
-             scoreDeduction += difference * scoreDeductionWeighting
-             isUserFrameGood = true
-           } else {
-             isUserFrameGood = false
-             break
-           }
-         }
-
+          let difference = Double(abs(userVal - idealVal))
+          if (difference <= tolerance) {
+            scoreDeduction += difference * scoreDeductionWeighting * Double(iterations) / 10
+            isUserFrameGood = true
+          } else {
+            isUserFrameGood = false
+            break
+          }
+        }
+        
         if isUserFrameGood {
           print("We found frame \(currentIdealFrameIndex)")
           currentIdealFrameIndex += 1
           score -= Double(scoreDeduction)
         }
       }
+      iterations += 1
+    }
+    
     if currentIdealFrameIndex < idealWorkout!.count {
-        // means that we couldn't find any user poses that resembles the ideal poses
-        // user has bad pose, might need to ask them to try again
-        print("User has bad pose, cannot match user poses to ideal poses")
-        score = 0.0
-      } else {
-        print("Hey we have a score of: \(score)")
-      }
+      // means that we couldn't find any user poses that resembles the ideal poses
+      // user has bad pose, might need to ask them to try again
+      print("User has bad pose, cannot match user poses to ideal poses")
+      score = 0.0
+    } else {
+      print("Hey we have a score of: \(score)")
+    }
     self.workoutResult = WorkoutResult(
       score: score,
       incorrectJoints: [0],
@@ -81,7 +86,7 @@ class WorkoutSession {
     )
   }
   
-  func save() -> Bool {
+  func save() {
     print(self.workoutResult)
     let workoutSessionItem = WorkoutSessionModel(
       imuData: self.imuData,
@@ -96,11 +101,10 @@ class WorkoutSession {
     Amplify.DataStore.save(workoutSessionItem) { result in
       switch(result) {
       case .success(let savedItem):
-          print("Saved workout session: \(savedItem.id)")
+        print("Saved workout session: \(savedItem.id)")
       case .failure(let error):
-          print("Could not save workout session to DataStore: \(error)")
+        print("Could not save workout session to DataStore: \(error)")
       }
     }
-    return true
   }
 }
